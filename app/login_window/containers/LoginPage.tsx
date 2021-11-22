00707f88d59edfa2ae22b015b7028f6da581e511
hexadecibal <hex#github@telios.io>
2021-11-22 11:29:55 -0500

    Recovery phrase during registration is now selectable

--- app/login_window/containers/LoginPage.tsx
+++ app/login_window/containers/LoginPage.tsx
@@ -69,9 +69,10 @@ class LoginPage extends Component<Props, State> {
 
     return (
       <div
-        className={`absolute w-full h-full flex flex-col overflow-hidden ${styles.container}`}
+        className={`absolute w-full h-full flex flex-col overflow-hidden`}
       >
         <div className="h-full w-full">
+          <div className={`absolute h-16 w-full ${styles.drag}`} />
           {activeWindow === 'first-screen' && (
             <FirstTimeScreen
               isLoading={isLoading}
