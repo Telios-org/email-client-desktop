8b1f925348140880fc663f50f52a14557d48680c
hexadecibal <hex#github@telios.io>
2021-12-03 16:38:54 -0500

    Catch error when migrate happens on null record

--- app/models/migrations/email/01_fix_case_sensitive_email.js
+++ app/models/migrations/email/01_fix_case_sensitive_email.js
@@ -2,16 +2,21 @@ const { Mailbox } = require('../../mailbox.model');
 
 module.exports = {
   up: async query => {
-    Mailbox.findOne({ 
-      where: { 
-        mailboxId: 1 
-      } 
-    }).then(mailbox => {
+    try {
+      const mailbox = await Mailbox.findOne({ 
+        where: { 
+          mailboxId: 1 
+        } 
+      })
+
       // Sanitize mailboxes that were created with capitalized chars
       if(mailbox && mailbox.address) {
         return mailbox.update({ address: mailbox.address.toLowerCase() });
       }
-    });
+
+    } catch(e) {
+      // Mailbox does not exist
+    }
   },
   down: async query => {
   }
