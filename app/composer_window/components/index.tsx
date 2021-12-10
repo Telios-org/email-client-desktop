8d434398196a627464926ebe584d75b37d62d520
Youri Noel Nelson <youri.nelson@gmail.com>
2021-12-10 16:28:41 -0500

    trying to fix linux path issue again and again

--- app/composer_window/components/index.tsx
+++ app/composer_window/components/index.tsx
@@ -1,6 +1,6 @@
 import Editor from './editor/Editor';
 import MessageInputs from './messageInputs/MessageInputs';
-import ComposerTopBar from './topBar/ComposerTopBar';
-import Attachments from './attachments/Attachments';
+import ComposerTopBar from './topBar/ComposerTopBar.tsx';
+import Attachments from './attachments/Attachments.tsx';
 
 export { Editor, MessageInputs, ComposerTopBar, Attachments };
