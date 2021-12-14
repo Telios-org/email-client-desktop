bb42e58e5ede6a2d3adedf398560789aee09dc3c
Youri Noel Nelson <youri.nelson@gmail.com>
2021-12-14 11:38:59 -0500

    attempt at path solve part 3

--- app/composer_window/components/index.tsx
+++ app/composer_window/components/index.tsx
@@ -1,6 +1,6 @@
 import Editor from './editor/Editor';
 import MessageInputs from './messageInputs/MessageInputs';
-import TopBar from './topBar/TopBar';
-import Attachments from './attachments/Attachments';
+import TopBar from './TopBar/TopBar';
+import Attachments from './Attachments/Attachments';
 
 export { Editor, MessageInputs, TopBar, Attachments };
