c837f13105f6cb66993741111d8cad1d4e13266b
hexadecibal <hex#github@telios.io>
2022-09-26 14:35:52 -0400

    Sync fix issues

--- app/services/messageIngress.service.js
+++ app/services/messageIngress.service.js
@@ -23,16 +23,16 @@ class MessageIngressService extends EventEmitter {
     this.MAX_RETRY = 1;
     this.folderCounts = {};
 
-    // channel.on('account:newMessage', async m => {
-    //   const { data } = m;
+    channel.on('account:newMessage', async m => {
+      const { data } = m;
 
-    //   this.msgBatchSize += 1;
+      this.msgBatchSize += 1;
 
-    //   channel.send({
-    //     event: 'messageHandler:newMessage',
-    //     payload: { meta: data.meta }
-    //   });
-    // });
+      channel.send({
+        event: 'messageHandler:newMessage',
+        payload: { meta: data.meta }
+      });
+    });
 
     channel.on('messageHandler:fileFetched', async m => {
       const { event, data, error } = m;
