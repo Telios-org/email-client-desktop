5aff3fb68d4060c006b2165a2a1a21f705e116b3
Youri Noel Nelson <youri.nelson@gmail.com>
2023-01-04 01:55:52 -0500

    TED-93 paywall

--- app/main_window/containers/Layout/SettingsPage/SettingsPage.tsx
+++ app/main_window/containers/Layout/SettingsPage/SettingsPage.tsx
@@ -11,7 +11,7 @@ import {
   Scan,
   Work
 } from 'react-iconly';
-import { DeviceMobileIcon } from '@heroicons/react/outline';
+import { ExclamationIcon } from '@heroicons/react/outline';
 import BrowserView, { removeViews } from 'react-electron-browser-view';
 import { setTimeout } from 'timers';
 import { retrieveStats } from '../../../actions/account/account';
@@ -25,34 +25,41 @@ import {
 } from '../../../components/Settings';
 import useCollectionListeners from '../../../../utils/hooks/useCollectionListeners';
 
+import { Button } from '../../../../global_components/button';
+
 const electron = require('electron');
 
 const tabs = [
   {
+    id:'general',
     name: 'General',
     component: GeneralPanel,
     icon: Setting,
     typeRestriction: null
   }, // { name: 'Notifications', panel: GeneralPanel },
   {
+    id:'billing',
     name: 'Plan & Billing',
     component: BillingPayments,
     icon: Wallet,
     typeRestriction: 'PRIMARY'
   },
   {
+    id:'security',
     name: 'Security',
     component: SecurityPanel,
     icon: ShieldDone,
     typeRestriction: null
   },
   {
+    id:'devices',
     name: 'Devices',
     component: DevicesPanel,
     icon: Scan,
     typeRestriction: null
   },
   {
+    id:'domains',
     name: 'Custom Domains',
     component: CustomDomains,
     icon: Work,
@@ -71,6 +78,7 @@ const SettingsPage = () => {
   const [showOverlay, setShowOverlay] = useState(false);
   const [browserURL, setBrowserURL] = useState('');
   const account = useSelector(state => state.account);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   useEffect(() => {
     // Retrieving the Updated Account Stats from Telios Server
@@ -87,6 +95,13 @@ const SettingsPage = () => {
     setBrowserURL(url);
     setShowOverlay(true);
   };
+
+  const onUpgrade = () => {
+    const index = tabs.findIndex(object => {
+      return object.id === 'billing';
+    });
+    setSelectedIndex(index);
+  }
 
   return (
     <div className="relative h-full w-full">
@@ -151,7 +166,11 @@ const SettingsPage = () => {
         </div>
       )}
       <div className="w-full h-full flex flex-row max-w-screen-2xl">
-        <Tab.Group vertical>
+        <Tab.Group
+          vertical
+          selectedIndex={selectedIndex}
+          onChange={setSelectedIndex}
+        >
           <div className="w-60 min-w-60 bg-white border-r border-gray-200">
             <nav aria-label="Sidebar" className="sticky">
               <div className="px-6 pt-6 pb-4">
@@ -205,37 +224,90 @@ const SettingsPage = () => {
               </Tab.List>
             </nav>
           </div>
-          <main className="ml-8 pr-8 py-8 flex-1 relative overflow-y-scroll h-full">
-            <div className="px-4 sm:px-0 h-full">
-              <Tab.Panels className="h-full">
-                {tabs
-                  .filter(
-                    f =>
-                      f.typeRestriction === account.type ||
-                      f.typeRestriction === null
-                  )
-                  .map(tab => {
-                    const tabProps = {};
+          <div className="relative flex-1">
+            {tabs[selectedIndex].id === 'domains' &&
+              ![
+                'PREMIUM',
+                'ULTIMATE',
+                'BUSINESS',
+                'FOUNDER',
+                'APPSUMOT1',
+                'APPSUMOT2'
+              ].includes(account.plan) && (
+                <div className="absolute w-full h-full">
+                  <div className="absolute w-full h-full bg-black/30 z-40 backdrop-grayscale" />
+                  <div className="absolute rounded-lg overflow-hidden bg-white w-1/2 z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <div className="relative h-full flex flex-col justify-between">
+                      <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <div className="sm:flex sm:items-start">
+                          <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-purple-100 sm:mx-0 sm:h-10 sm:w-10">
+                            <ExclamationIcon
+                              className="h-6 w-6 text-purple-600"
+                              aria-hidden="true"
+                            />
+                          </div>
+                          <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                            <h3 className="text-lg font-medium leading-6 text-gray-900">
+                              Custom Domains
+                            </h3>
+                            <div className="mt-2">
+                              <p className="text-sm text-gray-500">
+                                This feature requires a plan upgrade. Start
+                                using custom domains today.
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="bg-gray-50 px-4 py-3 sm:flex justify-center sm:px-6">
+                        <div className="w-fit">
+                          <Button
+                            type="button"
+                            onClick={onUpgrade}
+                            variant="secondary"
+                            className="pt-2 pb-2 text-sm font-medium inline-flex w-full justify-center"
+                          >
+                            Upgrade
+                          </Button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
 
-                    if (tab.name === 'Plan & Billing') {
-                      tabProps.handleOverlay = handleOverlay;
-                    }
-                    return (
-                      <Tab.Panel
-                        key={`panel_${tab.name}`}
-                        className="outline-none h-full"
-                      >
-                        <tab.component {...tabProps} />
-                      </Tab.Panel>
-                    );
-                  })}
-                {/* <div
+            <main className="ml-8 pr-8 py-8 flex-1 relative overflow-y-scroll h-full">
+              <div className="px-4 sm:px-0 h-full">
+                <Tab.Panels className="h-full">
+                  {tabs
+                    .filter(
+                      f =>
+                        f.typeRestriction === account.type ||
+                        f.typeRestriction === null
+                    )
+                    .map(tab => {
+                      const tabProps = {};
+
+                      if (tab.name === 'Plan & Billing') {
+                        tabProps.handleOverlay = handleOverlay;
+                      }
+                      return (
+                        <Tab.Panel
+                          key={`panel_${tab.name}`}
+                          className="outline-none h-full"
+                        >
+                          <tab.component {...tabProps} />
+                        </Tab.Panel>
+                      );
+                    })}
+                  {/* <div
                 style={{ height: '64rem' }}
                 className="border-2 border-dashed border-gray-300 rounded-lg"
               /> */}
-              </Tab.Panels>
-            </div>
-          </main>
+                </Tab.Panels>
+              </div>
+            </main>
+          </div>
         </Tab.Group>
       </div>
     </div>
